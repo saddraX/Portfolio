c439c1621b2b9a00c6b8b6a1eb3ed10131e34083
Michał Stasik <michal.stasik108@gmail.com>
2021-04-14 12:55:23 +0200

    Move files to SaveSystem folder

---
+++ SaveSystem/PlayerData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public float[] playerPosition;
+
+    //player stats
+    public float livesCount;
+    public bool isDead;
+    public int continueCount;
+    public int collectablesCount;
+    public float timeSpent;
+    public bool isInCave;
+
+    public bool canUseUmbrella;
+
+    public PlayerData(Player player)
+    {
+        //player stats
+        livesCount = Player.livesCount;
+        isDead = Player.isDead;
+        continueCount = Player.continueCount;
+        collectablesCount = Player.collectablesCount;
+        timeSpent = Player.timeSpent;
+        isInCave = Player.isInCave;
+
+        canUseUmbrella = Player.canUseUmbrella;
+    }
+}
